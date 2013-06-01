31c12b02f8d83d34c395a9c569d023ce640cb115
m-mcgowan <mat.mcgowan+github+profile@gmail.com>
2013-06-01 15:05:09 +0200

    fix for missing param in quoted device output. Previously produced garbage.

--- DeviceManager.cpp
+++ DeviceManager.cpp
@@ -495,7 +495,7 @@ void printAttrib(Print& p, char c, int8_t val, bool first=false)
 		p.print(',');
 
 	char tempString[32]; // resulting string limited to 128 chars
-	sprintf_P(tempString, PSTR("\"%c\":%d"), val);
+	sprintf_P(tempString, PSTR("\"%c\":%d"), c, val);
 	p.print(tempString);
 }
 
