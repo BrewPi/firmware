37aa7c8c637801732effd867b34f52cff4fd0572
m-mcgowan <mat.mcgowan+github+profile@gmail.com>
2014-01-05 06:36:30 +0100

    moved non-trivial access methods to cpp file. saves 20 bytes. (and potentially more in future as we introduce Onewire switch sensors.)

--- DS2413.h
+++ DS2413.h
@@ -131,11 +131,13 @@ public:
 			else
 				result &= ~mask;
 			ok = channelWriteAll((uint8_t)result);
-		}
+		}		
 		return ok;
 	}
 	
-	void channelWriteAll(uint8_t values);
+	bool channelWriteAll(uint8_t values) {
+		return accessWrite(values);
+	}
 	
 	DeviceAddress& getDeviceAddress()
 	{
@@ -182,26 +184,7 @@ private:
 	 * b2: PIOB state
 	 * b3: PIOB output latch state
 	 */
-	byte accessRead(uint8_t maxTries=3) /* const */
-	{		
-		#define ACCESS_READ 0xF5
-		
-		oneWire->reset();
-		oneWire->select(address);
-		oneWire->write(ACCESS_READ);
-		
-		bool success = false;
-		uint8_t data;
-		do 
-		{
-			data = oneWire->read();
-			success = (data>>4)==(!data&0xF);
-			data &= 0xF;
-		} while (!success && maxTries-->0);
-		
-		oneWire->reset();		
-		return success ? data : data|0x80;
-	}
+	byte accessRead(uint8_t maxTries=3);
 	
 	/*
 	 * Writes the state of all PIOs in one operation.
@@ -209,36 +192,7 @@ private:
 	 * /param maxTries the maximum number of attempts before giving up.
 	 * /return true on success
 	 */
-	bool accessWrite(uint8_t b, uint8_t maxTries=3)
-	{
-		#define ACCESS_WRITE 0x5A
-		#define ACK_SUCCESS 0xAA
-		#define ACK_ERROR 0xFF
-		
-		b |= 0xFC;   		/* Upper 6 bits should be set to 1's */
-		uint8_t ack = 0;
-		do
-		{
-			oneWire->reset();
-			oneWire->select(address);
-			oneWire->write(ACCESS_WRITE);
-			oneWire->write(b);
-			
-			/* data is sent again, inverted to guard against transmission errors */
-			oneWire->write(~b);
-			/* Acknowledgement byte, 0xAA for success, 0xFF for failure. */
-			ack = oneWire->read();
-						
-			if (ack==ACK_SUCCESS)
-				oneWire->read();		// status byte sent after ack
-			
-			//out.print("tries "); out.print(maxTries); out.print(" ack ");out.print(ack, HEX);out.print(" newValues ");out.print(newSettings, HEX);
-			//out.println();
-		} while (ack!=ACK_SUCCESS && maxTries-->0);
-		
-		oneWire->reset();
-		return ack==ACK_SUCCESS;
- 	}
+	bool accessWrite(uint8_t b, uint8_t maxTries=3);
 
 	OneWire* oneWire;	
 	DeviceAddress address;	
