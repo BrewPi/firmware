5bf211af8a1e5d0315ed4bdd2ee6c88589c805b3
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2015-02-04 17:47:23 +0100

    renamed spark/devices -> spark/modules

--- controller.mk
+++ controller.mk
@@ -1,23 +1,25 @@
+here_files = $(patsubst $(SOURCE_PATH)/%,%,$(wildcard $(SOURCE_PATH)/$1,$2))
+
 INCLUDE_DIRS += $(SOURCE_PATH)/app/controller
 INCLUDE_DIRS += $(SOURCE_PATH)/app/devices
 INCLUDE_DIRS += $(SOURCE_PATH)/app/devices/OneWire
 INCLUDE_DIRS += $(SOURCE_PATH)/app/fallback
 INCLUDE_DIRS += $(SOURCE_PATH)/platform/wiring
 INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/Adafruit_ILI9341
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/Adafruit_mfGFX
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/BrewPiTouch
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/DS2408
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/DS2413
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/Display
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/EEPROM
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/LowPassFilter
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/OneWire
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/OneWireSwitch
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/ScrollBox
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/Ticks
-INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/devices/ValvesController
-
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/Adafruit_ILI9341
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/Adafruit_mfGFX
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/BrewPiTouch
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/DS2408
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/DS2413
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/Display
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/EEPROM
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/LowPassFilter
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/OneWire
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/OneWireSwitch
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/ScrollBox
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/Ticks
+INCLUDE_DIRS += $(SOURCE_PATH)/platform/spark/modules/ValvesController
 
 CSRC += $(call target_files,app/controller,*.c)
 CPPSRC += $(call target_files,app/controller,*.cpp)
@@ -28,8 +30,8 @@ CPPSRC += $(call target_files,app/devices,*.cpp)
 CSRC += $(call target_files,platform/wiring/,*.c)
 CPPSRC += $(call target_files,platform/wiring/,*.cpp)
 
-CSRC += $(call target_files,platform/spark/,*.c)
-CPPSRC += $(call target_files,platform/spark/,*.cpp)
+CSRC += $(call target_files,platform/spark/modules,*.c)
+CPPSRC += $(call target_files,platform/spark/modules,*.cpp)
 
-SRC_EGUI = $(SOURCE_PATH)/platform/spark/devices/eGUI
+SRC_EGUI = $(SOURCE_PATH)/platform/spark/modules/eGUI
 include $(SRC_EGUI)/egui.mk
