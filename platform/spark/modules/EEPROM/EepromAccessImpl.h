f618262b1dbe4de28bab8fdd014d2e30718c0a27
Elco Jacobs <github@brewpi.com>
2015-02-19 21:10:10 +0100

    Reverse accidentally deleted code from eeprom abstraction

--- platform/spark/modules/EEPROM/EepromAccessImpl.h
+++ platform/spark/modules/EEPROM/EepromAccessImpl.h
@@ -1,20 +1,30 @@
 #pragma once
 
-// TODO - delegate to either EEPROM. emulation or flashee library
+#include "flashee-eeprom.h"
 
 class SparkEepromAccess
 {
+    Flashee::FlashDevice* flash;
 public:
-	static uint8_t readByte(eptr_t offset) {
-		return 0;
+    SparkEepromAccess() 
+    {
+        flash = Flashee::Devices::createAddressErase(0, 4096*32);
 	}
-	static void writeByte(eptr_t offset, uint8_t value) {
 		
+    uint8_t readByte(eptr_t offset) {
+        uint8_t value;
+        readBlock(&value, offset, 1);
+        return value;
+    }
+    void writeByte(eptr_t offset, uint8_t value) {
+        writeBlock(offset, &value, 1);
 	}
 	
-	static void readBlock(void* target, eptr_t offset, uint16_t size) {
+    void readBlock(void* target, eptr_t offset, uint16_t size) {
+        flash->read(target, offset, size);
 	}
-	static void writeBlock(eptr_t target, const void* source, uint16_t size) {
+    void writeBlock(eptr_t target, const void* source, uint16_t size) {
+        flash->write(source, target, size);
 	}	
 };
 
