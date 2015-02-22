a4060dc1a3611780e1fd2718edaf44aa5ea05a57
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2015-02-22 01:20:30 +0100

    files missing from previous commit (SourceTree commit selector was set to modified rather than pending)

---
+++ platform/spark/modules/eGUI_screens/brewpi_boot_screen.h
@@ -0,0 +1,17 @@
+#ifndef BREWPI_BOOT_SCREEN_H
+#define	BREWPI_BOOT_SCREEN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+D4D_EXTERN_SCREEN(screen_boot);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* BREWPI_BOOT_SCREEN_H */
+
