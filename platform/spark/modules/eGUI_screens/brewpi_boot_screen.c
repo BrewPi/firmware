9dd2b7130a0e07e9bb779d7751e4610710330d58
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2015-02-27 23:02:04 +0100

    fixes previous gui issues. GUI now functional.

--- platform/spark/modules/eGUI_screens/brewpi_boot_screen.c
+++ platform/spark/modules/eGUI_screens/brewpi_boot_screen.c
@@ -8,36 +8,52 @@
 #include "spark_macros.h"
 #include <string.h>
 
-#define INACTIVE_TEMP_COLOR D4D_COLOR_RGB(24,24,24)
-#define ACTIVE_TEMP_COLOR D4D_COLOR_RGB(24,24,24)
+#define INACTIVE_BG_COLOR D4D_COLOR_RGB(24,24,24)
+#define ACTIVE_BG_COLOR D4D_COLOR_RGB(44,90,160)
+#define INACTIVE_FG_LOW_COLOR D4D_COLOR_RGB(64,64,64)
+#define ACTIVE_FG_LOW_COLOR D4D_COLOR_RGB(160,160,160)
+#define INACTIVE_FS_COLOR D4D_COLOR_RGB(128,128,128)
+#define ACTIVE_FG_COLOR D4D_COLOR_RGB(255,255,255)
+
 
 const WIDGET_COLOR_SCHEME color_scheme_device = {
-    D4D_COLOR_RGB(44,90,160),           // bkg active
-    D4D_COLOR_RGB(24,24,24),            // bkg disable
-    D4D_COLOR_RGB(44,90,160),           // focus
-    D4D_COLOR_RGB(44,90,160),           // capture
-    D4D_COLOR_RGB(192,192,192),
-    D4D_COLOR_RGB(128,128,128),
+    ACTIVE_BG_COLOR,           // bkg active
+    INACTIVE_BG_COLOR,            // bkg disable
+    ACTIVE_BG_COLOR,           // focus
+    ACTIVE_BG_COLOR,           // capture
+    D4D_COLOR_RGB(192,192,192), // fg
+    D4D_COLOR_RGB(128,128,128), // fg inactive
     D4D_COLOR_RGB(255,255,255),
     D4D_COLOR_RGB(255,255,255),    
 };
 
 const WIDGET_COLOR_SCHEME color_scheme_connection = {
-    D4D_COLOR_RGB(44,90,160),           // bkg active
-    D4D_COLOR_RGB(24,24,24),            // bkg disable
-    D4D_COLOR_RGB(44,90,160),           // focus
-    D4D_COLOR_RGB(44,90,160),           // capture
-    D4D_COLOR_RGB(160,160,160),         // fg
-    D4D_COLOR_RGB(64,64,64),            // fg disabled
-    D4D_COLOR_RGB(160,160,160),         // fg focus
-    D4D_COLOR_RGB(160,160,160),         // fg capture
+    ACTIVE_BG_COLOR,           // bkg active
+    INACTIVE_BG_COLOR,            // bkg disable
+    ACTIVE_BG_COLOR,           // focus
+    ACTIVE_BG_COLOR,           // capture
+    ACTIVE_FG_LOW_COLOR,         // fg
+    INACTIVE_FG_LOW_COLOR,            // fg disabled
+    ACTIVE_FG_LOW_COLOR,         // fg focus
+    ACTIVE_FG_LOW_COLOR,         // fg capture
 };
 
 void ActuatorClicked(D4D_OBJECT* pThis);
 void SetActuatorButtonState(D4D_OBJECT* pThis, D4D_BOOL state)
 {
     D4D_SetText(pThis, state ? "ON" : "OFF");
-    pThis->clrScheme->bckg = state ? D4D_COLOR_RGB(44,90,160) : D4D_COLOR_RGB(24,24,24);    
+    D4D_COLOR bg = state ? color_scheme_device.bckg : color_scheme_device.bckgDis;
+    D4D_COLOR fg = state ? color_scheme_device.fore : color_scheme_device.foreDis;
+    
+    pThis->clrScheme->bckg = bg;
+    pThis->clrScheme->bckgDis = bg;
+    pThis->clrScheme->bckgCapture = bg;
+    pThis->clrScheme->bckgFocus = bg;
+    pThis->clrScheme->fore = fg;
+    pThis->clrScheme->foreDis = fg;
+    pThis->clrScheme->foreCapture = fg;
+    pThis->clrScheme->foreFocus = fg;
+
     D4D_InvalidateObject(pThis, D4D_TRUE);
 }
 
@@ -63,17 +79,20 @@ D4D_DECLARE_STD_PICTURE(scrBoot_bmpLogo, 10, 50, 50, 50, &bmp_brewpi_logo_48_30)
 #define _D4D_DECLARE_ACTUATOR(name, idx, x, y, cx, cy, font_label, font_item) \
     char name##text[5] = ""; \
     D4D_CLR_SCHEME name##scheme; \
-    _D4D_DECLARE_BUTTON(D4D_CONST, name, name##text, x, y, cx, cy, 0, NULL, NULL, D4D_BTN_F_DEFAULT, NULL, NULL, &name##scheme, FONT_ARIAL7_BIG, NULL, ActuatorClicked, NULL);
+    _D4D_DECLARE_BUTTON(D4D_CONST, name, name##text, x, y, cx, cy, 0, NULL, NULL, (D4D_OBJECT_F_VISIBLE | D4D_OBJECT_F_ENABLED | D4D_OBJECT_F_TOUCHENABLE | D4D_OBJECT_F_FASTTOUCH ), NULL, NULL, &name##scheme, FONT_ARIAL7_BIG, NULL, ActuatorClicked, NULL);
 
 
 D4D_DECLARE_ACTUATOR(1);
 D4D_DECLARE_ACTUATOR(2);
 D4D_DECLARE_ACTUATOR(3);
 
-
-D4D_DECLARE_CDV_LIST(D4D_CONST, scrBoot_devices0, 0, 90, 104, 150, FONT_ARIAL7_BIG, FONT_ARIAL7, &color_scheme_device, &color_scheme_connection);
-D4D_DECLARE_CDV_LIST(D4D_CONST, scrBoot_devices1, 108, 90, 104, 150, FONT_ARIAL7_BIG, FONT_ARIAL7, &color_scheme_device, &color_scheme_connection);
-D4D_DECLARE_CDV_LIST(D4D_CONST, scrBoot_devices2, 216, 90, 104, 150, FONT_ARIAL7_BIG, FONT_ARIAL7, &color_scheme_device, &color_scheme_connection);
+#define SCRBOOT_DEVICES_Y 90
+#define SCRBOOT_DEVICES_CX 104
+#define SCRBOOT_DEVICES_CY 150 
+#define SCRBOOT_DEVICES_CX_GAP 4
+D4D_DECLARE_CDV_LIST(D4D_CONST, scrBoot_devices0, 0, SCRBOOT_DEVICES_Y, SCRBOOT_DEVICES_CX, SCRBOOT_DEVICES_CY, FONT_ARIAL7_BIG, FONT_ARIAL7, &color_scheme_device, &color_scheme_connection);
+D4D_DECLARE_CDV_LIST(D4D_CONST, scrBoot_devices1, SCRBOOT_DEVICES_CX+SCRBOOT_DEVICES_CX_GAP, SCRBOOT_DEVICES_Y, SCRBOOT_DEVICES_CX, SCRBOOT_DEVICES_CY, FONT_ARIAL7_BIG, FONT_ARIAL7, &color_scheme_device, &color_scheme_connection);
+D4D_DECLARE_CDV_LIST(D4D_CONST, scrBoot_devices2, 2*(SCRBOOT_DEVICES_CX+SCRBOOT_DEVICES_CX_GAP), SCRBOOT_DEVICES_Y, SCRBOOT_DEVICES_CX, SCRBOOT_DEVICES_CY, FONT_ARIAL7_BIG, FONT_ARIAL7, &color_scheme_device, &color_scheme_connection);
 
 D4D_DECLARE_STD_SCREEN_BEGIN(screen_boot, ScreenBoot_)
     D4D_DECLARE_SCREEN_OBJECT(scrBoot_actuator1)
