6092308b2e07237294b8612f4788fca6ea3c6f53
Elco Jacobs <github@brewpi.com>
2015-03-01 22:48:02 +0100

    Start with lastUpdated at -1000 so first update is immediate

--- app/controller/Brewpi.cpp
+++ app/controller/Brewpi.cpp
@@ -94,9 +94,9 @@ void setup()
 
 void brewpiLoop(void)
 {
-	static unsigned long lastUpdate = 0;
+	static unsigned long lastUpdate = -1000; // init at -1000 to update immediately
 	uint8_t oldState;
-    ui.ticks();
+        ui.ticks();
 	if(ticks.millis() - lastUpdate >= (1000)) { //update settings every second
 		lastUpdate = ticks.millis();
 			
