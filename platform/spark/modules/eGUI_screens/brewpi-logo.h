846f9c2bb33d2f99a162c4aaaa19f5de3b31b8a5
Elco Jacobs <github@brewpi.com>
2015-03-03 11:26:44 +0100

    Added logo in 68x48

--- platform/spark/modules/eGUI_screens/brewpi-logo.h
+++ platform/spark/modules/eGUI_screens/brewpi-logo.h
@@ -4,5 +4,6 @@
 D4D_EXTERN_BMP(bmp_brewpi_logo_black_124_80);
 D4D_EXTERN_BMP(bmp_brewpi_logo_black_90_60);
 D4D_EXTERN_BMP(bmp_brewpi_logo_black_45_30);
+D4D_EXTERN_BMP(bmp_brewpi_logo_black_68_48);
 
 #endif
