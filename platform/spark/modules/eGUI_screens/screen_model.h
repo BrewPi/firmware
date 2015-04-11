5e789861581f25d920c4dee17b88a6dbf793acbf
Elco Jacobs <github@brewpi.com>
2015-04-11 16:20:25 +0200

    Updated license headers and removed LPGL headers added by mistake

--- platform/spark/modules/eGUI_screens/screen_model.h
+++ platform/spark/modules/eGUI_screens/screen_model.h
@@ -1,24 +1,21 @@
-/**
- ******************************************************************************
- * @file    screen_model.h
- * @authors Matthew McGowan
- ******************************************************************************
-  Copyright (c) 2015 Brewpi/Matthew McGowan.  All rights reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation, either
-  version 3 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************
- */
+/*
+ * Copyright 2015 BrewPi / Elco Jacobs, Matthew McGowan.
+ *
+ * This file is part of BrewPi.
+ * 
+ * BrewPi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BrewPi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BrewPi.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
 
 #ifndef SCREEN_MODEL_H
 #define	SCREEN_MODEL_H
