4a3ea5eafa68b25f9a9560e929c3611b4f7f3327
Elco Jacobs <elcojacobs@gmail.com>
2015-10-02 20:24:57 +0200

    Implemented a setpoint actuator: offsets a target setpoint from a reference setpoint.

---
+++ app/devices/Actuator/SetPointActuator.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2015 BrewPi/Elco Jacobs.
+ *
+ * This file is part of BrewPi.
+ *
+ * BrewPi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BrewPi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BrewPi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ #pragma once
+
+#include "Actuator.h"
+#include "SetPoint.h"
+#include "defaultDevices.h"
+ 
+/*
+ * A linear actuator that sets a setpoint to reference setpoint + actuator value
+ */
+class SetPointActuator : public LinearActuator
+{
+public:
+    SetPointActuator(SetPoint * targ = &defaultSetPoint, // setpoint to manipulate
+                     const SetPoint * ref = &defaultSetPoint, //setpoint to offset from
+                     temp_t mini = temp_t::min(), // minimum actuator value (targ - ref)
+                     temp_t maxi = temp_t::max()){ // maximum actuator value
+        target = targ;
+        reference = ref;
+        minimum = mini;
+        maximum = maxi;
+    }
+
+    void setActive(bool active) {
+        // do nothing, doesn't mean anything for a SetPoint Actuator
+    }
+    bool isActive() const{
+        temp_t referenceTemp = reference->read();
+        if(referenceTemp.isDisabledOrInvalid()){
+            return false;
+        }
+        return target->read() != reference->read();
+    }
+
+    void setValue(temp_t const& val) {
+        temp_t offset = val;
+        if(offset < minimum){
+            offset = minimum;
+        }
+        else if(offset > maximum){
+            offset = maximum;
+        }
+        target->write(reference->read() + offset);
+    }
+
+    temp_t readValue() const{
+        return target->read() - reference->read();
+    }
+
+    temp_t min() const{
+        return minimum;
+    }
+
+    temp_t max() const{
+        return maximum;
+    }
+
+private:
+    SetPoint * target;
+    const SetPoint * reference;
+    temp_t minimum;
+    temp_t maximum;
+};
---
+++ boost_test/SetPointActuatorTest.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2015 BrewPi/Elco Jacobs.
+ *
+ * This file is part of BrewPi.
+ *
+ * BrewPi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BrewPi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BrewPi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "SetPointActuator.h"
+#include "defaultDevices.h"
+#include "runner.h"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(SetPointActuatorTest)
+
+BOOST_AUTO_TEST_CASE(set_value){
+    SetPointSimple target(20.0);
+    SetPointSimple reference(20.0);
+
+    SetPointActuator act(&target, &reference);
+
+    act.setValue(10.0);
+
+    BOOST_REQUIRE_EQUAL(reference.read(), temp_t(20.0));
+    BOOST_REQUIRE_EQUAL(target.read(), temp_t(30.0));
+    BOOST_REQUIRE_EQUAL(act.readValue(), temp_t(10.0));
+
+    act.setValue(-10.0);
+    BOOST_REQUIRE_EQUAL(reference.read(), temp_t(20.0));
+    BOOST_REQUIRE_EQUAL(target.read(), temp_t(10.0));
+    BOOST_REQUIRE_EQUAL(act.readValue(), temp_t(-10.0));
+}
+
+BOOST_AUTO_TEST_CASE(min_max){
+    SetPointSimple target(20.0);
+    SetPointSimple reference(20.0);
+
+    SetPointActuator act(&target, &reference, -10.0, 10.0);
+
+    act.setValue(20.0);
+
+    BOOST_REQUIRE_EQUAL(reference.read(), temp_t(20.0));
+    BOOST_REQUIRE_EQUAL(target.read(), temp_t(30.0));
+    BOOST_REQUIRE_EQUAL(act.readValue(), temp_t(10.0));
+
+    act.setValue(-20.0);
+    BOOST_REQUIRE_EQUAL(reference.read(), temp_t(20.0));
+    BOOST_REQUIRE_EQUAL(target.read(), temp_t(10.0));
+    BOOST_REQUIRE_EQUAL(act.readValue(), temp_t(-10.0));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
