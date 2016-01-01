8a0d6e063eccb4a353df23175fe4863618d6f93d
Elco Jacobs <github@brewpi.com>
2016-01-01 18:59:39 +0100

    do not use macros for serialization in mixins. the macro uses pid->p as name instead of p

--- app/controller/ControllerMixins.cpp
+++ app/controller/ControllerMixins.cpp
@@ -1,36 +1,55 @@
+/*
+ * Copyright 2016 BrewPi/Elco Jacobs.
+ * Copyright 2016 Matthew McGowan.
+ *
+ * This file is part of BrewPi.
+ *
+ * BrewPi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BrewPi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BrewPi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "ControllerMixins.h"
 #include "Pid.h"
 #include "TempSensor.h"
 #include "TempSensorBasic.h"
 
-void PidMixin::serialize(JSON::Adapter& adapter) {
-    Pid* pid = (Pid*)this;
+void PidMixin::serialize(JSON::Adapter & adapter)
+{
+    Pid * pid = (Pid *) this;
+
     JSON::Class root(adapter, "Pid");
-    std::string name(getName()); // get name as std string for json_writer
+    std::string name(getName());    // get name as std string for json_writer
     JSON_E(adapter, name);
-    JSON_E(adapter, pid->enabled);
-    JSON_E(adapter, pid->setPoint);
-    JSON_E(adapter, pid->inputSensor);
-
-    JSON_E(adapter, pid->inputError);
-    JSON_E(adapter, pid->Kp);
-    JSON_E(adapter, pid->Ti);
-    JSON_E(adapter, pid->Td);
-    JSON_E(adapter, pid->p);
-    JSON_E(adapter, pid->i);
-    JSON_E(adapter, pid->d);
-
-    JSON_E(adapter, pid->actuatorIsNegative);
-    JSON_T(adapter, pid->outputActuator);
+    JSON::stream(adapter, "enabled", pid -> enabled, true);
+    JSON::stream(adapter, "setPoint", pid -> setPoint, true);
+    JSON::stream(adapter, "inputSensor", pid -> inputSensor, true);
+    JSON::stream(adapter, "inputError", pid -> inputError, true);
+    JSON::stream(adapter, "Kp", pid -> Kp, true);
+    JSON::stream(adapter, "Ti", pid -> Ti, true);
+    JSON::stream(adapter, "Td", pid -> Td, true);
+    JSON::stream(adapter, "p", pid -> p, true);
+    JSON::stream(adapter, "i", pid -> i, true);
+    JSON::stream(adapter, "d", pid -> d, true);
+    JSON::stream(adapter, "actuatorIsNegative", pid -> actuatorIsNegative, true);
+    JSON::stream(adapter, "outputActuator", pid -> outputActuator, false);
 }
 
+void TempSensorMixin::serialize(JSON::Adapter & adapter)
+{
+    TempSensor * sensor = (TempSensor *) this;
 
-void TempSensorMixin::serialize(JSON::Adapter& adapter) {
-    TempSensor* sensor = (TempSensor*)this;
     JSON::Class root(adapter, "TempSensor");
-    std::string name(sensor->getName()); // get name as std string for json_writer
+    std::string name(sensor -> getName());    // get name as std string for json_writer
     JSON_E(adapter, name);
-    JSON_T(adapter, sensor->sensor);
+    JSON::stream(adapter, "sensor", sensor -> sensor, false);
 }
-
-
