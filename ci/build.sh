a51bba6e7a2420956e518ba307b3f26f8dced3c3
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-01-02 09:36:54 +0100

    build and run boost tests

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,25 @@
+function status()
+{
+if [[ "$1" -eq 0 ]]; then
+  echo "✓ SUCCESS"
+else
+  echo "✗ FAILED"
+  exit 1
+fi
+}
+
+pushd boost_test
+make
+result=$?
+popd
+status $result
 
 pushd platform/spark 
 ./build-all.sh
-ex=$?
+result=$?
 popd
 
-exit $ex
+status $result
+
+./boost_test/obj/runner
+status $?
