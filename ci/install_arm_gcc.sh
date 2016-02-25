41b9c40affcafa90b14558f3c4ac5023f9c1e5e5
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-02-26 00:51:36 +0100

    changes package name for the updated PPA

--- ci/install_arm_gcc.sh
+++ ci/install_arm_gcc.sh
@@ -1,5 +1,5 @@
 
 sudo add-apt-repository -y ppa:team-gcc-arm-embedded/ppa &&
 sudo apt-get -qq update &&
-sudo apt-get -qq install  "gcc-arm-none-eabi" &&
+sudo apt-get -qq install  "gcc-arm-embedded" &&
 arm-none-eabi-gcc --version ;
