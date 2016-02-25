dd47f71cc79f837d2dff975b3b7e66ad14ab2b11
Elco Jacobs <elcojacobs@gmail.com>
2016-02-25 16:04:41 +0100

    Also render with ReadTheDocs theme in local build

--- docs/conf.py
+++ docs/conf.py
@@ -298,6 +298,17 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 
+# on_rtd is whether we are on readthedocs.org
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
 def setup(app):
     app.add_config_value('recommonmark_config', {
             'url_resolver': lambda url: github_doc_root + url,
