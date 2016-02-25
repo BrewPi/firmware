adebb49df3b76b0d468a5b3c6f8d778b22a709f0
Elco Jacobs <github@brewpi.com>
2016-02-25 01:24:51 +0100

    use default theme for docs and changed a few titles in conf

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -202,7 +202,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'brewpi_firmware_docsdoc'
+htmlhelp_basename = 'brewpi_firmware_doc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -224,7 +224,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'brewpi_firmware.tex', 'brewpi\\_firmware\\_docs Documentation',
+    (master_doc, 'brewpi_firmware.tex', 'BrewPi Firmware Documentation',
      'BrewPi', 'manual'),
 ]
 
@@ -254,7 +254,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'brewpi_firmware', 'brewpi_firmware Documentation',
+    (master_doc, 'brewpi_firmware', 'BrewPi Firmware Documentation',
      [author], 1)
 ]
 
@@ -268,8 +268,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'brewpi_firmware', 'brewpi_firmware Documentation',
-     author, 'brewpi_firmware', 'One line description of project.',
+    (master_doc, 'brewpi_firmware', 'BrewPi Firmware Documentation',
+     author, 'brewpi_firmware', 'Developer documentation for the BrewPi Firmware.',
      'Miscellaneous'),
 ]
 
