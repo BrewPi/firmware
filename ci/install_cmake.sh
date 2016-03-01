fa061d402cd89373209060ce9f92407b7b9d3f6a
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-03-01 08:32:12 +0100

    override gcc version to 4.9

--- ci/install_cmake.sh
+++ ci/install_cmake.sh
@@ -7,7 +7,7 @@ mkdir -p .cache/cmake
     wget --no-check-certificate http://cmake.org/files/v3.4/$file -O $dl
 )
 tar -xzf $dl                        
-if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
+export CXX="g++-4.9" CC="gcc-4.9"
 export cmake=./cmake-3.4.3-Linux-x86_64/bin/cmake
 
 
