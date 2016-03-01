464c5f89a86315fc1300c36dd7709e8b1be23512
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-03-01 08:58:52 +0100

    adds verbose make output

--- ci/build.sh
+++ ci/build.sh
@@ -11,5 +11,5 @@ fi
 : ${CC:=gcc}
 : ${CXX:=g++}
 : ${cmake:=cmake}
-$cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX . && make
+$cmake  -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX . && make VERBOSE=1
 
