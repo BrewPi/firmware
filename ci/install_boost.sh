f0018461654f6ee60b6eeb6580723cb3eaf71c74
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-03-01 07:13:48 +0100

    fixes cached boost installer

--- ci/install_boost.sh
+++ ci/install_boost.sh
@@ -1,12 +1,13 @@
 BOOST_VERSION=1_59_0
 url=https://github.com/boostorg/boost/archive/boost-1.59.0.tar.gz
 [[ -d .cache ]] || mkdir .cache
-test -f .cache/boost_$BOOST_VERSION.tar.gz || wget $url -O .cache/boost_$BOOST_VERSION.tar.gz 
+dl=.cache/boost_$BOOST_VERSION.tar.gz
+test -f $dl || wget $url -O $dl 
 export BOOST_HOME=$HOME/.ci/boost
 export BOOST_ROOT=$BOOST_HOME/boost_$BOOST_VERSION
 mkdir -p $BOOST_HOME
 test -d $BOOST_ROOT || (
-   tar zxf boost_$BOOST_VERSION.tar.gz
+   tar zxf $dl 
    mv boost_$BOOST_VERSION  $BOOST_HOME
 ) 
 test -d $BOOST_ROOT || ( echo "boost root $BOOST_ROOT not created." && exit 1)
