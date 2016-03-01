8a9a866ba3b60d89c2c63f5b5aaefd32b2065b0b
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-03-01 08:22:20 +0100

    caches cmake in a separate directory. upgrades to 4.9 gcc and bumps cmake required version back up to 3.4.3

--- ci/install_cmake.sh
+++ ci/install_cmake.sh
@@ -1,8 +1,13 @@
 file=cmake-3.4.3-Linux-x86_64.tar.gz
+dl=.cache/cmake/$file
+mkdir -p .cache/cmake
 
-[[  -f .cache/$file ]] ||  wget --no-check-certificate http://cmake.org/files/v3.4/$file -O .cache/$file
-tar -xzf .cache/$file                        
-if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
+[[  -f $dl ]] || (
+    rm .cache/cmake/*
+    wget --no-check-certificate http://cmake.org/files/v3.4/$file -O $dl
+)
+tar -xzf $dl                        
+if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
 export cmake=./cmake-3.4.3-Linux-x86_64/bin/cmake
 
 
