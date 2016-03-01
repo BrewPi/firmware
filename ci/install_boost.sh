ce87153aeab219de95b40906d10987203a67957c
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-03-01 07:28:41 +0100

    installs boost to a cache directory and clears out that directory if the file has to be downloaded. This clears old versions from the cache.

--- ci/install_boost.sh
+++ ci/install_boost.sh
@@ -1,8 +1,11 @@
 BOOST_VERSION=1_59_0
-url=https://github.com/boostorg/boost/archive/boost-1.59.0.tar.gz
-[[ -d .cache ]] || mkdir .cache
-dl=.cache/boost_$BOOST_VERSION.tar.gz
-test -f $dl || wget $url -O $dl 
+url=http://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_$BOOST_VERSION.tar.gz
+mkdir -p .cache/boost
+dl=.cache/boost/boost_$BOOST_VERSION.tar.gz
+test -f $dl || (
+   rm .cache/boost/* 
+   wget $url -O $dl
+) 
 export BOOST_HOME=$HOME/.ci/boost
 export BOOST_ROOT=$BOOST_HOME/boost_$BOOST_VERSION
 mkdir -p $BOOST_HOME
