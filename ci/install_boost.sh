179ad000cf102bac63ff38c0873a31f95085d0d2
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-05-27 06:34:21 +0200

    update boost version to 1.61.0

--- ci/install_boost.sh
+++ ci/install_boost.sh
@@ -1,9 +1,9 @@
-BOOST_VERSION=1_59_0
+BOOST_VERSION=1_61_0
 
-test -f boost_$BOOST_VERSION.tar.gz || wget http://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_$BOOST_VERSION.tar.gz 
+test -f boost_$BOOST_VERSION.tar.gz || wget http://downloads.sourceforge.net/project/boost/boost/1.61.0/boost_$BOOST_VERSION.tar.gz 
 export BOOST_HOME=$HOME/.ci/boost
 export BOOST_ROOT=$BOOST_HOME/boost_$BOOST_VERSION
-mkdir -p $BOOST_HOME
+mkdir -p $BOOST_HOME:
 test -d $BOOST_ROOT || (
    tar zxf boost_$BOOST_VERSION.tar.gz
    mv boost_$BOOST_VERSION  $BOOST_HOME
