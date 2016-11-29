dc4daeef17a9ea6e5d21ea04c78bfa8989ef5522
Elco Jacobs <elco@brewpi.com>
2016-11-29 11:11:47 +0100

    added actuator reference class

---
+++ lib/inc/ActuatorReferences.h
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2015 Matthew McGowan
+ * Copyright 2015 BrewPi/Elco Jacobs.
+ *
+ * This file is part of BrewPi.
+ * 
+ * BrewPi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BrewPi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BrewPi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include "ActuatorInterfaces.h"
+#include "defaultDevices.h"
+
+/*
+ * An ActuatorDigitalReference holds a pointer to a digital actuator
+ * when the actuator is not set (null) it points to the default actuator
+ */
+class ActuatorDigitalReference : public virtual ActuatorDigital
+{
+public:
+    ActuatorDigitalReference() : target(nullptr) {};
+    ~ActuatorDigitalReference() = default;
+
+    uint8_t type() const override {
+        return ACTUATOR_TOGGLE;
+    };
+
+    void setActive(bool active) override {
+        ref()->setActive(active);
+    }
+
+    bool isActive() const override {
+        return ref()->isActive();
+    }
+
+    void setTarget(ActuatorDigital * act){
+        target = act;
+    }
+
+    void unsetTarget(){
+        target = nullptr;
+    }
+
+private:
+    ActuatorDigital * ref() const {
+        if (target == nullptr){
+           return defaultActuator();
+        }
+        return target;
+    }
+
+    ActuatorDigital * target;
+};
+
+
+/*
+ * An ActuatorRangeReference holds a pointer to a range (linear) actuator
+ * when the actuator is not set (null) it points to the default linear actuator
+ */
+class ActuatorRangeReference : public virtual ActuatorRange
+{
+public:
+    ActuatorRangeReference() : target(nullptr) {};
+    ~ActuatorRangeReference() = default;
+
+    uint8_t type() const override {
+        return ACTUATOR_RANGE;
+    };
+
+    void setValue(temp_t const& val) {
+        ref()->setValue(val);
+    }
+    temp_t getValue() const override {
+        return ref()->getValue();
+    }
+    temp_t readValue() const override {
+        return ref()->readValue();
+    }
+    temp_t min() const override {
+        return ref()->min();
+    }
+    temp_t max() const override {
+        return ref()->max();
+    }
+
+    void setTarget(ActuatorRange * act){
+        target = act;
+    }
+
+    void unsetTarget(){
+        target = nullptr;
+    }
+
+private:
+    ActuatorRange * ref() const {
+        if (target == nullptr){
+           return defaultLinearActuator();
+        }
+        return target;
+    }
+
+    ActuatorRange * target;
+};
+
