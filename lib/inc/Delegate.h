f16c661e6bec931704a374bab271682a3f7561fb
Elco Jacobs <elco@brewpi.com>
2017-02-25 22:29:17 +0100

    factored out generic delegate class

--- lib/inc/Delegate.h
+++ lib/inc/Delegate.h
@@ -2,17 +2,17 @@
  * Copyright 2017 BrewPi/Elco Jacobs.
  *
  * This file is part of BrewPi.
- * 
+ *
  * BrewPi is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * BrewPi is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with BrewPi.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,31 +20,31 @@
 #pragma once
 
 #include <stdint.h>
-#include "ControllerMixins.h"
-#include "Interface.h"
-#include "Delegate.h"
+#include "RefTo.h"
 
-class SetPointDelegate :
-    public SetPoint,
-    public Delegate<SetPoint>,
-    public SetPointDelegateMixin
+template<typename T>
+class Delegate
 {
 public:
-    SetPointDelegate() = default;
-    SetPointDelegate(std::function<Interface * ()> lookup) : Delegate<SetPoint>(lookup){}
-    ~SetPointDelegate() = default;
+    Delegate() = default;
+    Delegate(std::function<Interface * ()> lookup) {
+        delegate.setLookup(lookup);
+    }
+    virtual ~Delegate() = default;
 
-    void accept(VisitorBase & v) final {
-        v.visit(*this);
+
+    void setLookup(std::function<Interface* ()> lookup) {
+        delegate.setLookup(lookup);
     }
 
-    temp_t read() const final {
-        return delegate().read();
+    void unsetLookup() {
+        delegate.setLookup(nullptr);
     }
 
-    void write(temp_t val) final {
-        delegate().write(val);
+    std::function<Interface * ()> getLookup() {
+        return delegate.getLookup();
     }
 
-friend class SetPointDelegateMixin;
+protected:
+    RefTo<T> delegate;
 };
