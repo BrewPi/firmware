25d6303ede71de9b59a4cde0ad39b4cfb10167c1
Elco Jacobs <elco@brewpi.com>
2017-08-21 11:43:56 +0200

    Workaround for channel switching bug: disconnect wifi if IP address is 0

--- app/controller/PiLink.cpp
+++ app/controller/PiLink.cpp
@@ -91,6 +91,11 @@ public:
             currentStream = &Serial;
         }
         else if (WiFi.ready()) {
+            if(WiFi.localIP()[0]==0){
+                // workaround for bug where WiFi.ready() returns true with IP 0.0.0.0
+                WiFi.disconnect();
+                return 0;
+            }
             if(!tcpServerRunning) {
                 tcpServer.begin();
                 tcpServerRunning = true;
