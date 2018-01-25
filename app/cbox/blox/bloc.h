0cc9e0e5299ba3b4bdcbabc547bcca73256a1558
Elco Jacobs <elco@brewpi.com>
2018-01-25 21:05:11 +0100

    connect cbox update to app object update

--- app/cbox/blox/bloc.h
+++ app/cbox/blox/bloc.h
@@ -3,10 +3,17 @@
 #include "../EepromAwareWritableValue.h"
 #include "../nanopb_callbacks.h"
 
-class Bloc : public EepromAwareWritableValue {
+class Bloc : virtual public EepromAwareWritableValue {
 	Interface * getApplicationInterface() override final{
 		return getApplicationInterfaceImpl();
 	}
 
 	virtual Interface * getApplicationInterfaceImpl() = 0;
+
+	virtual void update() override final {
+	    Interface * appInterface = getApplicationInterface();
+	    if(appInterface != nullptr){
+	        appInterface->update();
+	    }
+	}
 };
