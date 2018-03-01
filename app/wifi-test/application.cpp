56b7889f87fa5d09808216b0eca4dac00c49faf7
Elco Jacobs <elco@brewpi.com>
2018-03-01 09:12:30 +0100

    Simplified handling of TCP server now that WiFi.ready() is working as expected

--- app/wifi-test/application.cpp
+++ app/wifi-test/application.cpp
@@ -7,35 +7,29 @@ SerialLogHandler traceLog(LOG_LEVEL_TRACE);
 
 static TCPServer tcpServer = TCPServer(6666);
 static TCPClient tcpClient;
+bool tcpServerRunning;
 
 // Toggle LED pin to see that application loop is not blocked.
 // You could use D7 when testing with a Photon.
 // I'm using another pin here, because D7 is one of the SWD pins used by the debugger
 const int LED_PIN = P1S0;
 
-enum tcp_state_enum {
-    RUNNING_FINE,
-    NEEDS_TO_STOP,
-    STOPPED,
-    ALLOWED_TO_RESTART,
-};
-
-volatile uint8_t tcp_state;
 
 void stopTcp(){
     tcpServer.stop();
     tcpClient.stop();
+    tcpServerRunning = false;
     Serial.print("TCP server stopped\n");
 }
 
 void startTcp(){
     tcpServer.begin();
+    tcpServerRunning = true;
     Serial.print("TCP server started\n");
 }
 
 
 void handle_network_events(system_event_t event, int param){
-    static int cloud_connection_tries = 0;
     switch(param){
     case network_status_powering_on:
         break;
@@ -46,15 +40,8 @@ void handle_network_events(system_event_t event, int param){
     case network_status_off:
         break;
     case network_status_connecting:
-        // set a flag to restart the TCP server, outside of the system thread
-        // restarting it here doesn't work
-        // leaving it running doesn't work
-        if(tcp_state != tcp_state_enum::STOPPED){
-            tcp_state = tcp_state_enum::NEEDS_TO_STOP;
-        }
         break;
     case network_status_connected:
-        tcp_state = tcp_state_enum::ALLOWED_TO_RESTART;
         break;
     default:
         break;
@@ -75,6 +62,7 @@ void handle_cloud_events(system_event_t event, int param){
     default:
         break;
     }
+    Serial.printf("cloud event %d\n", param);
 }
 
 // return time that has passed since timeStamp, take overflow into account
@@ -102,43 +90,25 @@ void setup() {
 
     System.on(network_status, handle_network_events);
     System.on(cloud_status, handle_cloud_events);
-    tcp_state = tcp_state_enum::ALLOWED_TO_RESTART;
 }
 
 void loop() {
     static system_tick_t last_update = millis();
     static system_tick_t lastLedToggle = millis();
 
-    if(WiFi.RSSI() >= 0){
+    if(!WiFi.ready()){
         // WiFi is in error state, stop TCP server
-        if(tcp_state != tcp_state_enum::STOPPED){
-            tcp_state = tcp_state_enum::NEEDS_TO_STOP;
+        if(tcpServerRunning){
+            stopTcp();
         }
     }
-
-    switch(tcp_state){
-        case tcp_state_enum::RUNNING_FINE:
-            if(!WiFi.ready()){
-                // check in case WiFi is not ready but the system events didn't trigger the correct state
-                tcp_state = tcp_state_enum::NEEDS_TO_STOP;
-            }
-            break;
-        case tcp_state_enum::NEEDS_TO_STOP:
-            Serial.print("Stopping TCP\n");
-            stopTcp();
-            tcp_state = tcp_state_enum::STOPPED;
-            break;
-        case tcp_state_enum::STOPPED:
-            break;
-        case tcp_state_enum::ALLOWED_TO_RESTART:
-            Serial.print("Restarting TCP\n");
-            startTcp();
-            tcp_state = tcp_state_enum::RUNNING_FINE; // assume it is running fine afterwards
-            break;
-
+    else{
+    	if(!tcpServerRunning){
+			startTcp();
+		}
     }
 
-    if(tcp_state == tcp_state_enum::RUNNING_FINE){
+    if(tcpServerRunning){
         if(tcpClient.status()){
             bool noErrors = true;
             while (noErrors && tcpClient.available() > 0) {
