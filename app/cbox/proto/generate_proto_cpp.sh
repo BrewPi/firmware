a541cf93f8afc4051155ab5ab10a53644920f99b
Elco Jacobs <elcojacobs@gmail.com>
2018-06-11 15:10:39 +0200

    detect mingw platform in proto generation shell script to append .bat

--- app/cbox/proto/generate_proto_cpp.sh
+++ app/cbox/proto/generate_proto_cpp.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 PROTO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NANOPB_PATH="${PROTO_DIR}/../../../platform/spark/firmware/nanopb/nanopb"
-PROTOC_NANOPB_PLUGIN="${NANOPB_PATH}/generator/protoc-gen-nanopb" # add .bat when on windows
+PROTOC_NANOPB_PLUGIN="${NANOPB_PATH}/generator/protoc-gen-nanopb"
+if [[ "${OSTYPE}" == "msys" ]]; then
+	PROTOC_NANOPB_PLUGIN="${PROTOC_NANOPB_PLUGIN}.bat" # add .bat when on windows
+fi
 PROTOC_INCLUDE_PATH="-I${PROTO_DIR} -I${NANOPB_PATH}/generator -I${NANOPB_PATH}/generator/proto"
 
 ls ${PROTOC_NANOPB_PLUGIN}
