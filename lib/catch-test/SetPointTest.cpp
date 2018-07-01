bc9a49c5e8d982eaa9ffe21a4928758d1abf2937
Øyvind Hvamstad <oyvind@hvamstad.net>
2018-07-02 00:11:15 +0200

    Add setpoint tests

--- lib/catch-test/SetPointTest.cpp
+++ lib/catch-test/SetPointTest.cpp
@@ -19,3 +19,50 @@ SCENARIO("Basic test of a set point", "[setpoint]") {
         }
     }
 }
+
+SCENARIO("foo", "[setpoint]") {
+    SetPointMinMax sp;
+
+    sp.setMin(10.0);
+    sp.setMax(30.0);
+    
+    WHEN("value is written within the range of max and min") {
+        sp.write(15.0);
+
+        THEN("the set value is read") {
+            REQUIRE(sp.read() == temp_t(15.0));
+        }
+    }
+
+    WHEN("value is set above the range of max and min") {
+        sp.write(40.0);
+
+        THEN("the max value is read") {
+            REQUIRE(sp.read() == temp_t(30.0));
+        }
+    }
+    
+    WHEN("value is set below the range of max and min") {
+        sp.write(5.0);
+
+        THEN("the min value is read") {
+            REQUIRE(sp.read() == temp_t(10.0));
+        }
+    }
+}
+
+SCENARIO("Default set point is immutable", "[setpoint]") {
+    SetPoint * sp = defaultSetPoint();
+
+    REQUIRE(sp->read() == temp_t::invalid());
+
+    WHEN("temperature is written") {
+        sp->write(temp_t(21.1));
+
+        THEN("the value does not change") {
+            REQUIRE(sp->read() == temp_t::invalid());
+        }
+    }
+
+    delete sp;
+}
