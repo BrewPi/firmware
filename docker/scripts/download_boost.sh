60b34945f11e2ed9a1525c08dfd0aa61d2880398
Elco Jacobs <elco@brewpi.com>
2018-07-09 09:30:39 +0200

    Revert to boost 1.65 to not get __cpp_noexcept_function_type warnings and link boost statically

--- docker/scripts/download_boost.sh
+++ docker/scripts/download_boost.sh
@@ -1,3 +1,3 @@
-url=http://downloads.sourceforge.net/project/boost/boost/1.67.0/boost_$BOOST_VERSION.tar.gz
+url=http://downloads.sourceforge.net/project/boost/boost/1.65.0/boost_$BOOST_VERSION.tar.gz
 mkdir -p $BOOST_HOME 
 curl -sSL ${url} | tar -xz -C $BOOST_HOME
