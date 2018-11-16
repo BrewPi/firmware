065fc66c94e716c654310140e956c182c4c54194
Elco Jacobs <elco@brewpi.com>
2018-11-16 12:34:06 +0100

    Fix missing Link postfix after changing proto files

--- app/brewblox/blox/BlockInterfaces.cpp
+++ app/brewblox/blox/BlockInterfaces.cpp
@@ -17,63 +17,63 @@ template <>
 const obj_type_t
 interfaceIdImpl<ProcessValue<safe_elastic_fixed_point<11, 12, int32_t>>>()
 {
-    return BrewbloxFieldOptions_LinkType_ProcessValue;
+    return BrewbloxFieldOptions_LinkType_ProcessValueLink;
 }
 
 template <>
 const obj_type_t
 interfaceIdImpl<TempSensor>()
 {
-    return BrewbloxFieldOptions_LinkType_TempSensor;
+    return BrewbloxFieldOptions_LinkType_TempSensorLink;
 }
 
 template <>
 const obj_type_t
 interfaceIdImpl<SetpointSensorPair>()
 {
-    return BrewbloxFieldOptions_LinkType_SetpointSensorPair;
+    return BrewbloxFieldOptions_LinkType_SetpointSensorPairLink;
 }
 
 template <>
 const obj_type_t
 interfaceIdImpl<Setpoint>()
 {
-    return BrewbloxFieldOptions_LinkType_Setpoint;
+    return BrewbloxFieldOptions_LinkType_SetpointLink;
 }
 
 template <>
 const obj_type_t
 interfaceIdImpl<ActuatorAnalogConstrained>()
 {
-    return BrewbloxFieldOptions_LinkType_ActuatorAnalog;
+    return BrewbloxFieldOptions_LinkType_ActuatorAnalogLink;
 }
 
 template <>
 const obj_type_t
 interfaceIdImpl<ActuatorDigitalConstrained>()
 {
-    return BrewbloxFieldOptions_LinkType_ActuatorDigital;
+    return BrewbloxFieldOptions_LinkType_ActuatorDigitalLink;
 }
 
 template <>
 const obj_type_t
 interfaceIdImpl<TimedMutex>()
 {
-    return BrewbloxFieldOptions_LinkType_Mutex;
+    return BrewbloxFieldOptions_LinkType_MutexLink;
 }
 
 template <>
 const obj_type_t
 interfaceIdImpl<Balancer<blox_DigitalConstraint_mutex_tag>>()
 {
-    return BrewbloxFieldOptions_LinkType_Balancer;
+    return BrewbloxFieldOptions_LinkType_BalancerLink;
 }
 
 template <>
 const obj_type_t
 interfaceIdImpl<OneWireDevice>()
 {
-    return BrewbloxFieldOptions_LinkType_OneWireDevice;
+    return BrewbloxFieldOptions_LinkType_OneWireDeviceLink;
 }
 
 } // end namespace cbox
